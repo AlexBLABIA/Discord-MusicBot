e50cac93726c5be9aa5163736d7fbecae68c4f22
AlexBLABIA <128096156+AlexBLABIA@users.noreply.github.com>
2025-11-09 03:49:47 +0100

    Update Lavalink server configuration details

--- config.js
+++ config.js
@@ -25,9 +25,9 @@ module.exports = {
 	// Lavalink server; public lavalink -> https://lavalink-list.darrennathanael.com/; create one yourself -> https://darrennathanael.com/post/how-to-lavalink
 	nodes: [
 		{
-			identifier: "Muzykant lavalink v4", //- Used for indentifier in stats commands.
-			password: "youshallnotpass", //- The password of the lavalink server.
-			host: "lavalink.jirayu.net", //- The host name or IP of the lavalink server.
+			identifier: "TriniumHost Lavalink v4 SSL", //- Used for indentifier in stats commands.
+			password: "free", //- The password of the lavalink server.
+			host: "lavalink-v4.triniumhost.com", //- The host name or IP of the lavalink server.
 			port: 443, // The port that lavalink is listening to. This must be a number!
 			retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
 			retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
