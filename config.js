e17dc9f43f418130f66cc3b9303e4cd0bed3d1e6
AlexBLABIA <128096156+AlexBLABIA@users.noreply.github.com>
2025-11-09 03:57:36 +0100

    Clear Lavalink server credentials in config.js
    Removed sensitive information from Lavalink configuration.

--- config.js
+++ config.js
@@ -26,8 +26,8 @@ module.exports = {
 	nodes: [
 		{
 			identifier: "", //- Used for indentifier in stats commands.
-			password: "https://dsc.gg/ajidevserver", //- The password of the lavalink server.
-			host: lava-v4.ajieblogs.eu.org, //- The host name or IP of the lavalink server.
+			password: "", //- The password of the lavalink server.
+			host: "", //- The host name or IP of the lavalink server.
 			port: 443, // The port that lavalink is listening to. This must be a number!
 			retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
 			retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
