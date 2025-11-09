4d71aebecd2d2cf09dc950889c87d61196f11450
AlexBLABIA <128096156+AlexBLABIA@users.noreply.github.com>
2025-11-09 03:31:25 +0100

    Update Lavalink node identifier and host

--- config.js
+++ config.js
@@ -25,11 +25,11 @@ module.exports = {
 	// Lavalink server; public lavalink -> https://lavalink-list.darrennathanael.com/; create one yourself -> https://darrennathanael.com/post/how-to-lavalink
 	nodes: [
 		{
-			identifier: "AjieDev-V3", //- Used for indentifier in stats commands.
-			host: "lava-v3.ajieblogs.eu.org", //- The host name or IP of the lavalink server.
+			identifier: "AjieDev-V4", //- Used for indentifier in stats commands.
+			host: "lava-v4.ajieblogs.eu.org", //- The host name or IP of the lavalink server.
 			port: 443, // The port that lavalink is listening to. This must be a number!
 			password: "https://dsc.gg/ajidevserver", //- The password of the lavalink server.
-			retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
+			retryAmount: 443, //- The amount of times to retry connecting to the node if connection got dropped.
 			retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
 			secure: true, //- Can be either true or false. Only use true if ssl is enabled!
 		},
